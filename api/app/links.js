58a9d2280ef23b1719782e85144f92a75300e33f
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-10-27 14:10:52 +0600

    Implemented check for unique shortUrl

--- api/app/links.js
+++ api/app/links.js
@@ -10,9 +10,18 @@ router.post('/', async (req, res) => {
         return res.status(400).send({error: 'Data not valid'});
     }
 
+    let linkExists = true;
+    let url;
+
+    while (linkExists) {
+        url = nanoid(7);
+        const existingLinks = await Link.find({shortUrl: url});
+        linkExists = Boolean(existingLinks.length)
+    }
+
     const linkData = {
         originalUrl: req.body.originalUrl,
-        shortUrl: nanoid(7),
+        shortUrl: url
     };
 
     const link = new Link(linkData);
@@ -30,7 +39,8 @@ router.get('/:shortUrl', async (req, res) => {
         const link = await Link.findOne({shortUrl: req.params.shortUrl});
 
         if (link) {
-            res.send(link);
+            res.send({message: `Redirected to ${link.originalUrl}`});
+            res.status(301).redirect(link.originalUrl);
         } else {
             res.sendStatus(404).send({error: 'Link not found'});
         }
